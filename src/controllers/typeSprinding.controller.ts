040c134e7f461c8ab9810a8c05a675ed4763683b
cong2235 <115072482+cong2235@users.noreply.github.com>
2024-10-23 09:43:05 +0700

    TypeSprinding edit&delete

--- src/controllers/typeSprinding.controller.ts
+++ src/controllers/typeSprinding.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+﻿import { NextFunction, Request, Response } from 'express'
 import { TypeSprindingService } from '@src/services/typeSprinding.service'
 import { StatusCodes } from 'http-status-codes'
 const CreateTypeSprinding = async (
@@ -19,4 +19,48 @@ const CreateTypeSprinding = async (
 
 export const TypeSprindingController = {
   CreateTypeSprinding
+}
+
+// Sửa TypeSprinding
+const editTypeSprinding = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params
+    const { name, estimatedAmount, abbreviation, colorId, iconId } = req.body
+
+    const updatedTypeSprinding = await TypeSprindingService.updateTypeSprinding(id, {
+      name,
+      estimatedAmount,
+      abbreviation,
+      colorId,
+      iconId
+    })
+
+    res.status(StatusCodes.OK).json({
+      status: 'OK',
+      data: updatedTypeSprinding
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+// Xóa TypeSprinding
+const deleteTypeSprinding = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params
+    await TypeSprindingService.deleteTypeSprinding(id)
+
+    res.status(StatusCodes.OK).json({
+      status: 'OK',
+      message: `TypeSprinding with ID ${id} has been deleted`
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const TypeSprindingController = {
+  CreateTypeSprinding,
+  editTypeSprinding,
+  deleteTypeSprinding
 }
